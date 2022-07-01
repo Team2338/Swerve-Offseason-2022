1c986aa4ac4b43762de6ce256a04656190849138
DS-2 <billknopfjr@yahoo.com>
2022-06-30 19:41:45 -0500

    True Init

---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems;
+
+public class DrivetrainSubsystem {
+    public static final double MAX_VOLTAGE = 12.0;
+
+    public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 * SdsModuleConfigurations.MK3_STANDARD.get
+}
